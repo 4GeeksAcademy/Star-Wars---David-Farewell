eb7568809ca9ad804d8f244250bffd3a37a6b1f6
DavidFarewell <david.castillo.architect@gmail.com>
2025-03-26 20:32:10 +0000

    Star Wars Blog con React Router - David Farewell

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="custom-footer">
             <p>
-            "Que la Fuerza nos acompañó: ningún fan de Star Wars ni backend developer fue dañado en esta misión."
+            "Que la Fuerza os acompañe: ningún fan de Star Wars ni backend developer fue dañado en esta misión."
             </p>
         </footer>
     );
