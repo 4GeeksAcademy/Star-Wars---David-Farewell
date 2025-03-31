7d3f8a53e296fac15649beba31d208d1b751a161
DavidFarewell <david.castillo.architect@gmail.com>
2025-03-31 19:29:53 +0000

    Star Wars Blog - David Farewell

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -3,30 +3,35 @@ import { useEffect } from "react";
 
 const FavoriteButton = ({ item }) => {
     const { store, dispatch } = useGlobalReducer();
-    const isFavorite = store.favorites.some(fav => fav.uid === item.uid);
+
+    // con uid y type
+    const isFavorite = store.favorites.some(
+        fav => fav.uid === item.uid && fav.type === item.type
+    );
 
     useEffect(() => {
-        // LocalStorage 
         localStorage.setItem("favorites", JSON.stringify(store.favorites));
     }, [store.favorites]);
 
     const toggleFavorite = () => {
         let updatedFavorites;
+
         if (isFavorite) {
-            updatedFavorites = store.favorites.filter(fav => fav.uid !== item.uid);
+            updatedFavorites = store.favorites.filter(
+                fav => !(fav.uid === item.uid && fav.type === item.type)
+            );
             dispatch({ type: "REMOVE_FAVORITE", payload: item });
         } else {
             updatedFavorites = [...store.favorites, item];
             dispatch({ type: "ADD_FAVORITE", payload: item });
         }
 
-        // Guardar en localStorage
         localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
     };
 
     return (
         <button className={`btn-favorite ${isFavorite ? "active" : ""}`} onClick={toggleFavorite}>
-            {isFavorite ? "❤" : "❤"}
+            {isFavorite ? "❤️" : "🤍"}
         </button>
     );
 };
